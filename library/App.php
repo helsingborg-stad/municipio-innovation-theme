00e92be9c3814e99780ccfa64daf00e9c6d0950b
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-05-13 17:12:40 +0200

    Enqueue & set roboto font

--- library/App.php
+++ library/App.php
@@ -6,5 +6,12 @@ class App
     public function __construct()
     {
         new \InnovationsPortalen\Theme\Enqueue();
+        add_action('wp_enqueue_scripts', array($this, 'enqueueRobotoFont'));
+    }
+
+
+    public function enqueueRobotoFont()
+    {
+        wp_enqueue_style('roboto', 'https://fonts.googleapis.com/css?family=Roboto');
     }
 }
