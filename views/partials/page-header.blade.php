1f3966596ed7e59569964f20c32d39c600ada8c9
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-12-09 20:40:53 +0100

    page header styles

--- views/partials/page-header.blade.php
+++ views/partials/page-header.blade.php
@@ -1,12 +1,12 @@
-<header class="page-header">
+<header class="page-header page-header--{{get_post_type()}}">
     <div class="container page-header__container">
         <div class="grid u-flex-row-reverse@sm u-flex-row-reverse@md u-flex-row-reverse@lg u-flex-row-reverse@xl">
-            <div class="grid-xs-12 grid-sm-6 order-2 page-header__image">
-                <img src="{{municipio_get_thumbnail_source($post->ID, array(342,256), '4:3')}}">
+            <div class="grid-xs-12 grid-sm-6 order-2 page-header__image u-mb-2@xs">
+                <img src="{{municipio_get_thumbnail_source($post->ID, array(456,342), '4:3')}}">
             </div>
             <div class="grid-xs-12 grid-sm-6 page-header__body">
                 <div class="page-header__content">
-                    <span class="page-header__meta">{{get_post_type()}}</span>
+                    <span class="page-header__meta meta u-mb-1">{{get_post_type()}}</span>
                     <h1 class="page-header__title">{{the_title()}}</h1>
                     {{the_excerpt()}}
                 </div>
