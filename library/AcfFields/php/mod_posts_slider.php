376cb7155267520a0f4ba63430f55149b29f83f0
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-12-15 14:41:06 +0100

    Add fields content & archive link text fields

--- library/AcfFields/php/mod_posts_slider.php
+++ library/AcfFields/php/mod_posts_slider.php
@@ -3,7 +3,7 @@
 if (function_exists('acf_add_local_field_group')) {
     acf_add_local_field_group(array(
     'key' => 'group_5fd88a970d678',
-    'title' => __('Post Slider', 'municipio-innovation-theme'),
+    'title' => __('Innovation theme add-on', 'municipio-innovation-theme'),
     'fields' => array(
         0 => array(
             'key' => 'field_5fd88d9613316',
@@ -23,6 +23,44 @@
             'ui' => 1,
             'ui_on_text' => '',
             'ui_off_text' => '',
+        ),
+        1 => array(
+            'key' => 'field_5fd8945d21b96',
+            'label' => __('Module Heading Content', 'municipio-innovation-theme'),
+            'name' => 'module_heading_content',
+            'type' => 'textarea',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'placeholder' => '',
+            'maxlength' => '',
+            'rows' => 2,
+            'new_lines' => '',
+        ),
+        2 => array(
+            'key' => 'field_5fd8952821b97',
+            'label' => __('Custom archive link text', 'municipio-innovation-theme'),
+            'name' => 'custom_archive_link_text',
+            'type' => 'text',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+            'maxlength' => '',
         ),
     ),
     'location' => array(
