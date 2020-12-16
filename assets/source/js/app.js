88734d8565d33e1adfcbfd03633572e5fe1857a3
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-12-16 12:28:17 +0100

    add postSlider & flickity scripts

--- assets/source/js/app.js
+++ assets/source/js/app.js
@@ -1,7 +1,13 @@
 import smoothScroll from './smoothScroll';
 import './mobileMenu';
+import flickity from './flickity';
+import postSlider from './postSlider';
 
 document.addEventListener('DOMContentLoaded', function(event) {
+
+
     // Do stuff
     smoothScroll();
+    flickity();
+    postSlider();
   });
