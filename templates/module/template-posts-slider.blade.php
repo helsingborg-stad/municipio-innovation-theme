acd3e4fb9ad533e078527d5fcf27372fe66276c6
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-12-16 14:54:19 +0100

    implement circular buttons

--- templates/module/template-posts-slider.blade.php
+++ templates/module/template-posts-slider.blade.php
@@ -1,10 +1,24 @@
 @extends('template-posts')
 
-@section('after-archive-link')
-    <div class="grid-xs-12 u-text-right">
-        <div class="js-flickity-controller" data-flickity-controller-target="#flickity-mod-posts-{{$ID}}">
-            <button class="js-post-slider__prev"> <i class="pricon pricon-angle-left"></i></button>
-            <button class="js-post-slider__next"> <i class="pricon pricon-angle-right"></i></button>
+@section('archive-link')
+    <div class="u-ml-auto grid-xs-fit-content u-align-self-end">
+        <div class="grid">
+            @yield('before-archive-link')
+            
+            <div class="grid-xs-12 u-flex u-justify-content-end">
+                @if ($posts_data_source !== 'input' && isset($archive_link) && $archive_link && $archive_link_url)
+                        <a class="u-mr-3 related-posts__archive_link weight-400 u-align-self-center" href="{{ $archive_link_url }}?{{ http_build_query($filters) }}">{{$archiveLinkText}}</a>
+                @endif
+
+                <button class="btn btn-sm btn-outline btn-floating btn-floating--flat js-post-slider__prev u-mr-1">
+                    <i class="pricon pricon-angle-left"></i>
+                </button>
+                <button class="btn btn-sm btn-outline btn-floating btn-floating--flat js-post-slider__next">
+                    <i class="pricon pricon-angle-right"></i>
+                </button>
+            </div>
+
+            @yield('after-archive-link')
         </div>
     </div>
 @stop
