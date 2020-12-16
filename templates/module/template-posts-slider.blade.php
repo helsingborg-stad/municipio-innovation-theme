0b1f103ab16c5027996bc486fbdb23f7a90da6d3
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-12-16 15:31:45 +0100

    Hide items before init

--- templates/module/template-posts-slider.blade.php
+++ templates/module/template-posts-slider.blade.php
@@ -7,7 +7,7 @@
             
             <div class="grid-xs-12 u-flex u-justify-content-end">
                 @if ($posts_data_source !== 'input' && isset($archive_link) && $archive_link && $archive_link_url)
-                        <a class="u-mr-3 related-posts__archive_link weight-400 u-align-self-center" href="{{ $archive_link_url }}?{{ http_build_query($filters) }}">{{$archiveLinkText}}</a>
+                    <a class="u-mr-3 related-posts__archive_link weight-400 u-align-self-center" href="{{ $archive_link_url }}?{{ http_build_query($filters) }}">{{$archiveLinkText}}</a>
                 @endif
 
                 <button class="btn btn-sm btn-outline btn-floating btn-floating--flat js-post-slider__prev u-mr-1">
@@ -24,9 +24,9 @@
 @stop
 
 @section('loop')
-    <div id="flickity-mod-posts-{{$ID}}" class="grid u-w-100 js-post-slider__flickity" data-flickity-options='{!! $slider['flickityOptions'] !!}'  data-equal-container>
+    <div id="flickity-mod-posts-{{$ID}}" class="grid u-w-100 post-slider__flickity js-post-slider__flickity" data-flickity-options='{!! $slider['flickityOptions'] !!}'  data-equal-container>
         @foreach ($posts as $post)
-            <div class="flickity-item js-flickity-item {{ $posts_columns }}">
+            <div class="post-slider__item @if (isset($columnsPerRow) && $loop->iteration > $columnsPerRow) u-flickity-init-hidden @endif {{ $posts_columns }}">
                 @includeFirst(['item.post-' . $post->post_type, 'item.post'], ['post' => $post])
             </div>
         @endforeach
