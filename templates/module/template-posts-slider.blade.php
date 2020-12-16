3daeab666ea433494611b1aa589b02f7c9c0eb2a
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-12-16 12:30:19 +0100

    fix js-classes

--- templates/module/template-posts-slider.blade.php
+++ templates/module/template-posts-slider.blade.php
@@ -3,14 +3,14 @@
 @section('after-archive-link')
     <div class="grid-xs-12 u-text-right">
         <div class="js-flickity-controller" data-flickity-controller-target="#flickity-mod-posts-{{$ID}}">
-            <button class="js-flickity-prev"> <i class="pricon pricon-angle-left"></i></button>
-            <button class="js-flickity-next"> <i class="pricon pricon-angle-right"></i></button>
+            <button class="js-post-slider__prev"> <i class="pricon pricon-angle-left"></i></button>
+            <button class="js-post-slider__next"> <i class="pricon pricon-angle-right"></i></button>
         </div>
     </div>
 @stop
 
 @section('loop')
-    <div id="flickity-mod-posts-{{$ID}}" class="grid u-w-100" data-flickity-options='{!! $slider['flickityOptions'] !!}'  data-equal-container>
+    <div id="flickity-mod-posts-{{$ID}}" class="grid u-w-100 js-post-slider__flickity" data-flickity-options='{!! $slider['flickityOptions'] !!}'  data-equal-container>
         @foreach ($posts as $post)
             <div class="flickity-item js-flickity-item {{ $posts_columns }}">
                 @includeFirst(['item.post-' . $post->post_type, 'item.post'], ['post' => $post])
