53a3e156b67206ec14fa2d968bbcd11f754ec87d
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2021-06-22 13:04:31 +0200

    allow video instead of featured image

--- views/partials/page-header/center.blade.php
+++ views/partials/page-header/center.blade.php
@@ -13,8 +13,25 @@
                     @endif
                 </div>
             </div>
-            
-            @if (municipio_get_thumbnail_source($post->ID, array(1080,608), '16:9'))
+
+            @if(
+                function_exists('build_youtube_url')
+                && get_field('page_header_video_url', get_queried_object_id())
+                && $youtubeUrl = build_youtube_url(get_field('page_header_video_url', get_queried_object_id())))
+                    <div class="grid-xs-12 grid-md-10 offset-md-1 page-header__image u-mt-4 u-offset-page-header-img">
+                        <div class="u-yt-wrapper">
+                            <iframe 
+                                frameborder="0" 
+                                scrolling="no" 
+                                marginheight="0" 
+                                marginwidth="0" 
+
+                                type="text/html"
+                                src="{{$youtubeUrl}}">
+                            </iframe>
+                        </div>
+                    </div>
+            @elseif (municipio_get_thumbnail_source($post->ID, array(1080,608), '16:9'))
                 <div class="grid-xs-12 grid-md-10 offset-md-1 page-header__image u-mt-4 u-offset-page-header-img">
                     <img src="{{municipio_get_thumbnail_source($post->ID, array(1080,608), '16:9')}}">
                 </div>
